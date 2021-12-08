9c80b49c74d8974728924df1fb68c0758f87f2dd
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2021-12-08 19:45:37 +0530

    Day 8 one star done

--- Day 8/input.js
+++ Day 8/input.js
@@ -1,12 +1,19 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 const input = fs
-	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+	.readFileSync(path.join(__dirname, "input.txt"), "utf8")
 	.toString()
 	.trim()
-	.split('\n')
-	.map((inp) => inp);
+	.split("\n")
+	.map(line => {
+	  	return line.split("|").map(word => {
+	  		return word
+	  		  .trim()
+	  		  .split(" ")
+	  		  .map(signal => signal);
+	  	});
+	});
 
 module.exports = {
 	input,
